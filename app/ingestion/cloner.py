cfb1eea9e665f8efa0fd03ebd5b05e7ce22bc801
SHUMAIL AHMAD SAMAR <shumailahmad8938@gmail.com>
2026-05-09 22:27:07 +0530

    added logic for cloning repo locally

---
+++ app/ingestion/cloner.py
@@ -0,0 +1,29 @@
+import os
+import tempfile
+import git  
+
+
+IGNORE_DIRS = {
+    ".git",".github", "node_modules", "__pycache__", ".venv", "venv",
+    "dist", "build", ".next", ".nuxt", "target", "vendor",
+    ".idea", ".vscode", "eggs", ".eggs",".docs","site","venv"
+}
+ALLOWED_EXTENSIONS = {
+    ".py", ".js", ".ts", ".jsx", ".tsx", ".java", ".go",
+    ".rs", ".cpp", ".c", ".h", ".cs", ".rb", ".php",
+    ".swift", ".kt", ".scala", ".r", ".sh", ".yaml",
+    ".yml", ".toml", ".json", ".md", ".sql", ".html", ".css"
+}
+def clone_repo(github_url:str)-> tuple[str,list[str]]:
+    repo_dir=tempfile.mkdtemp()
+    git.Repo.clone_from(github_url,repo_dir,depth=1)
+    file_paths=[]
+    for root,dirnames,filenames in os.walk(repo_dir):
+        dirnames[:]=[d for d in dirnames if d not in IGNORE_DIRS]
+        for filename in filenames:
+            _,ext=os.path.splitext(filename)
+            if ext.lower()not in ALLOWED_EXTENSIONS:
+                continue
+            full_path=os.path.join(root,filename)
+            file_paths.append(full_path)
+    return repo_dir,file_paths
